9035a377bd5acf2b7c4924c6407e0f296e6b5d43
juzer1711 <juzer1711@gmail.com>
2026-05-21 02:01:57 -0500

    fix app vercel

--- api/index.ts
+++ api/index.ts
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://tu-frontend.vercel.app",
+      "https://salon-estudio-frontend.vercel.app",
     ],
     credentials: true,
   })
